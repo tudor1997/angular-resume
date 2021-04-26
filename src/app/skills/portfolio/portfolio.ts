07981185d3efdd794d944e0eb96c5addeca240f8
DESKTOP-OP0FSSP\anast <anastasiu.tudor@gmail.com>
2021-04-26 14:39:32 +0300

    hobbies section + new item for portfolio

--- src/app/skills/portfolio/portfolio.ts
+++ src/app/skills/portfolio/portfolio.ts
@@ -4,4 +4,5 @@ export const PORTFOLIO:Portfolio[] = [
   {name:'Music Store', img:'./assets/images/musicStore.png'},
   {name:'Music Player', img:'./assets/images/mp3Player.png'},
   {name:'Joke Teller', img:'./assets/images/jokeTeller.png'},
+  {name:'Random Image Feed', img:'./assets/images/imageFeed.png'},
 ]
